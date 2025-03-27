3da886404796e8407071c00bea499128891dbae9
JAEJUN0124 <jaejuno123w@gmail.com>
2025-03-27 13:07:04 +0900

    app click

--- lib/presentation/mt_app.dart
+++ lib/presentation/mt_app.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
       home: HomeScreen(),
     );
   }
