3f2e00ec741360ff94706824e00c6ef8d66378dc
JAEJUN0124 <jaejuno123w@gmail.com>
2025-04-18 12:38:41 +0900

    9주차 과제

--- lib/presentation/main/widgets/height_box.dart
+++ lib/presentation/main/widgets/height_box.dart
@@ -2,7 +2,9 @@ import 'package:daelim_2025/presentation/common/widgets/white_box.dart';
 import 'package:flutter/material.dart';
 
 class HeightBox extends StatefulWidget {
-  const HeightBox({super.key});
+  final Function(double height) onChanged;
+
+  const HeightBox({super.key, required this.onChanged});
 
   @override
   State<HeightBox> createState() => _HeightBoxState();
@@ -21,7 +23,7 @@ class _HeightBoxState extends State<HeightBox> {
           Text('Height (CM)', style: TextStyle(fontSize: 18)),
           // 숫자
           Text(
-            '$_height',
+            '${_height.toStringAsFixed(1)}',
             style: TextStyle(
               fontSize: 60,
               fontWeight: FontWeight.bold,
@@ -33,10 +35,12 @@ class _HeightBoxState extends State<HeightBox> {
             value: _height,
             min: 50.0,
             max: 300.0,
+            activeColor: Color(0xFF6C63FF),
             onChanged: (double value) {
               setState(() {
                 _height = value;
               });
+              widget.onChanged(_height);
             },
           ),
         ],
